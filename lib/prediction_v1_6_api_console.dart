70c6d4a9e6fed8cf766a0b1315645e626a8297b2
Adam Singer <financeCoding@gmail.com>
2014-09-13 18:56:30 -0700

    Automated update

--- lib/prediction_v1_6_api_console.dart
+++ lib/prediction_v1_6_api_console.dart
@@ -7,6 +7,7 @@ import 'package:google_prediction_v1_6_api/src/console_client.dart';
 import "package:google_prediction_v1_6_api/prediction_v1_6_api_client.dart";
 
 /** Lets you access a cloud hosted machine learning service that makes it easy to build smart apps */
+@deprecated
 class Prediction extends Client with ConsoleClient {
 
   /** OAuth Scope2: Manage your data and permissions in Google Cloud Storage */
